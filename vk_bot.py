05ade1e2aa686ce4a8fdc4fc5a64f64ba5fde478
RomanRVV <rvachyov.roma@yandex.ru>
2023-12-01 16:34:28 +0300

    add DialogFlow to VK bot

--- vk_bot.py
+++ vk_bot.py
@@ -3,12 +3,32 @@ import random
 import vk_api as vk
 from vk_api.longpoll import VkLongPoll, VkEventType
 from environs import Env
+import uuid
 
 
 def echo(event, vk_api):
+
+    from google.cloud import dialogflow
+
+    project_id = "gameofverbs-406212"
+
+    language_code = "ru"
+
+    session_client = dialogflow.SessionsClient()
+
+    session = session_client.session_path(project_id, str(uuid.uuid4()))
+
+    text_input = dialogflow.TextInput(text=event.text, language_code=language_code)
+
+    query_input = dialogflow.QueryInput(text=text_input)
+
+    response = session_client.detect_intent(
+        request={"session": session, "query_input": query_input}
+    )
+
     vk_api.messages.send(
         user_id=event.user_id,
-        message=event.text,
+        message=response.query_result.fulfillment_text,
         random_id=random.randint(1,1000)
     )
 
@@ -29,3 +49,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+    
