7da452a5a3aa4adca0314518c0ada62cc182fe1b
RomanRVV <rvachyov.roma@yandex.ru>
2023-12-01 16:42:23 +0300

    added skip for unknown words

--- vk_bot.py
+++ vk_bot.py
@@ -25,12 +25,12 @@ def echo(event, vk_api):
     response = session_client.detect_intent(
         request={"session": session, "query_input": query_input}
     )
-
-    vk_api.messages.send(
-        user_id=event.user_id,
-        message=response.query_result.fulfillment_text,
-        random_id=random.randint(1,1000)
-    )
+    if not response.query_result.intent.is_fallback:
+        vk_api.messages.send(
+            user_id=event.user_id,
+            message=response.query_result.fulfillment_text,
+            random_id=random.randint(1,1000)
+        )
 
 
 def main():
@@ -49,4 +49,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-    
