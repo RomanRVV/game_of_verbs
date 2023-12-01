eba46ddaefbd467f230ba2c022337d8e79547d25
RomanRVV <rvachyov.roma@yandex.ru>
2023-12-01 15:48:40 +0300

    setup DialogFlow

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import telegram
 from environs import Env
 
 import logging
+import uuid
 
 from telegram import Update, ForceReply
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
@@ -22,6 +23,29 @@ def echo(update: Update, context: CallbackContext):
     update.message.reply_text(update.message.text)
 
 
+def detect_intent_texts(update: Update, context: CallbackContext):
+
+    from google.cloud import dialogflow
+
+    project_id = "gameofverbs-406212"
+
+    language_code = "ru"
+
+    session_client = dialogflow.SessionsClient()
+
+    session = session_client.session_path(project_id, str(uuid.uuid4()))
+
+    text_input = dialogflow.TextInput(text=update.message.text, language_code=language_code)
+
+    query_input = dialogflow.QueryInput(text=text_input)
+
+    response = session_client.detect_intent(
+        request={"session": session, "query_input": query_input}
+    )
+
+    context.bot.send_message(chat_id=update.effective_chat.id, text=response.query_result.fulfillment_text)
+
+
 def main():
     env = Env()
     env.read_env()
@@ -32,7 +56,7 @@ def main():
     dispatcher = updater.dispatcher
 
     dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, detect_intent_texts))
 
     updater.start_polling()
     updater.idle()
